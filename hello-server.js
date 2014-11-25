c4df77ced4f2fd28f22d55b723f2092308687bd6
robertabrooks <robertb1@uw.edu>
2014-11-25 11:52:38 -0800

    demo

--- hello-server.js
+++ hello-server.js
@@ -6,4 +6,25 @@
 
 'use strict';
 
-var http = require('http');
+/*var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    //res.end('Hello World from Bobby!\n');
+    res.end('you requested ' + req.url);
+});*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World from Bobby!');
+});
+
+app.get('/foo', function(req, res) {
+    res.send('You requested /foo');
+});
+
+app.listen(8080, function() {
+    console.log('Server listening on http://localhost:8080');
+});
